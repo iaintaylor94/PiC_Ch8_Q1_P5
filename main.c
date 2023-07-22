835e8cdfef69ea93e879c515b4c08044a7923737
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-22 13:00:41 +0000

    copied program

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
+/* Function to find the greatet common divisor of two non-negative integers */
+
 #include <stdio.h>
 
+// Declare function
+void gcd (int, int);
+
 int main(void) {
-  printf("Hello World\n");
+
+  // Calculate and print the gcd of several integer pairs
+  gcd (150, 35);
+  gcd (1026, 405);
+  gcd (83, 240);
+  
   return 0;
+}
+
+// Define function
+void gcd (int u, int v) {
+  int temp;
+
+  // Print the Question
+  printf ("The gcd of %d and %d is ", u, v);
+
+  // Calculate gcd of u and v
+  while (v != 0) {
+    temp = u % v;
+    u = v;
+    v = temp;
+  }
+
+  // print gcd of u and v
+  printf ("%d\n", u);
 }
